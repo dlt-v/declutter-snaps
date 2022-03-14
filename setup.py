e0ef51995997c7172385270965b0909298bd1c8b
Tomek Michalski <tmm.michalski@gmail.com>
2022-03-14 17:31:08 +0100

    add initial setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from typing import List
+from setuptools import setup, find_packages
+
+
+def read_requirements() -> List[str]:
+    with open('requirements.txt') as req:
+        content = req.read()
+        return content.split('\n')
+
+
+setup(
+    name='declutter-snaps',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=read_requirements(),
+    entry_points="""
+        [console_scripts]
+        de-snap=de-snap.cli:cli
+    """,
+)
